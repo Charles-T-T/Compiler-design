8b5f5cdcd4fb0366b10b348a766688829a310ab1
charles_wsl <1592276395@qq.com>
2025-04-02 18:48:33 +0600

    finish lab1.

--- labs/lab1/src/run_lab1.sh
+++ labs/lab1/src/run_lab1.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+
+DEFAULT_INPUT="test-cases-pl0/1.pl0"
+INPUT_FILE="${1:-$DEFAULT_INPUT}"
+
 flex lab1.l
 gcc lex.yy.c -o lab1
-./lab1 < test-cases-pl0/1.pl0
+./lab1 < "$INPUT_FILE"
 
 rm lab1 lex.yy.c
